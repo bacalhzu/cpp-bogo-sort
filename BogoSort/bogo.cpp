5b952b500ba49763db03f459befba2e79e4ab427
littlesekii <littlesekii@gmail.com>
2024-01-22 18:59:08 -0300

    Divided sort implementation into functions.

--- BogoSort/bogo.cpp
+++ BogoSort/bogo.cpp
@@ -2,45 +2,41 @@
 
 namespace bogo
 {
+	bool isSorted(const int* vector, const size_t size)
+	{
+		//check if array is sorted
+		for (size_t i = 1; i < size; i++)
+		{
+			if (vector[i] < vector[i - 1])
+				return false;
+		}
+		return true;
+	}
+
+	void shuffle(int* vector, const size_t size)
+	{
+		//shuffle the array
+		for (size_t i = 0; i < size; i++)
+		{			
+			//get a random index between the current and last index
+			int randomIndex = (rand() % size - i) + i;
+
+			//stores de current value
+			int aux = vector[i];
+
+			//swap values
+			vector[i] = vector[randomIndex];
+			vector[randomIndex] = aux;
+		}
+	}
+
 	long long sort(int* vector, const size_t size)
 	{
 		srand(time(NULL));
-		rand();
-
 		int iterations{};
-		bool isSorted = false;
-		while (!isSorted)
+		while (!isSorted(vector, size))
 		{
-			bool testSort = true;
-
-			//check if array is sorted
-			for (size_t i = 1; i < size && testSort; i++)
-			{
-				if (vector[i] < vector[i - 1])
-					testSort = false;
-			}
-
-			//return if is sorted
-			if (testSort)
-			{
-				isSorted = true;
-			}
-			else
-			{
-				//shuffle the array
-				for (size_t i = 0; i < size; i++)
-				{
-					//get a random index between the current and last index
-					int randomIndex = (rand() % size - i) + i;
-
-					//stores de current value
-					int aux = vector[i];
-
-					//swap values
-					vector[i] = vector[randomIndex];
-					vector[randomIndex] = aux;
-				}
-			}
+			shuffle(vector, size);				
 			iterations++;
 		}	
 		return iterations;
--- BogoSort/bogo.h
+++ BogoSort/bogo.h
@@ -4,7 +4,23 @@
 namespace bogo
 {
 	/// <summary>
-	/// Shuffles the vector and test if the its values are sorted.
+	/// Test if the vector's values are sorted.
+	/// </summary>
+	/// <param name="vector"> - Vector to be sort.</param>
+	/// <param name="size"> - Vector size.</param>
+	/// <returns>If the vector is sorted</returns>
+	bool isSorted(const int* vector, const size_t size);
+
+	/// <summary>
+	/// Shuffles the vector's values.
+	/// </summary>
+	/// <param name="vector"> - Vector to be sort.</param>
+	/// <param name="size"> - Vector size.</param>
+	void shuffle(int* vector, const size_t size);
+
+
+	/// <summary>
+	/// Sort the vector.
 	/// </summary>
 	/// <param name="vector"> - Vector to be sort.</param>
 	/// <param name="size"> - Vector size.</param>
